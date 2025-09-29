003d14a6711ef7511c8ae29757816a3a4c0cf5f8
Tharun9772Gaming <tharun2015gamer@gmail.com>
2025-09-29 17:51:18 -0400

    Update and rename index.js to worker.js

---
+++ worker.js
@@ -0,0 +1,16 @@
+
+    const worker = new Worker('/worker.js');
+
+    fetch('/index.html')
+      .then(response => response.text())
+      .then(htmlContent => {
+        worker.postMessage({ type: 'html', content: htmlContent });
+      })
+      .catch(error => console.error('Error fetching HTML:', error));
+
+    self.onmessage = function(event) {
+      if (event.data.type === 'html') {
+        const htmlTemplate = event.data.content;
+        console.log('Opened Bloxcraft Game Hub :', htmlTemplate);
+      }
+    };
